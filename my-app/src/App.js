6284be5d040beb7be7a46cf6163a0e46ab9a0354
Peter Huynh <trinh1234533@yahoo.com>
2024-02-18 21:36:33 -0600

    Added 2 buttons and used a setState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 // const user = {
 //   name: 'Hedy Lamarr',
@@ -7,46 +8,57 @@ import './App.css';
 //   imageSize: 90,
 // }
 
-const products = [
-  {title: 'Cabbage', isFruit: false, id:1},
-  {title: 'Garlic', isFruit: false, id:2},
-  {title: 'Apple', isFruit: true, id:3},
-  
-];
+function MyButton(){
+  const [count, setCount] = useState(0);
 
-export default function ShoppingList(){
-  const listItems = products.map(product=>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'pink' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
+  function handleClick(){
+    setCount(count + 1);
+  }
+
+  return(
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
   );
-
-  return (
-    <ul>
-      {listItems}
-    </ul>
-  )
 }
-// function App() {
-//   return (
-//     <>
-//       <h1>{user.name}</h1>
-//       <img
-//         className="avatar"
-//         src={user.imageUrl}
-//         alt={'Photo of ' + user.name}
-//         style={{
-//           width: user.imageSize,
-//           height: user.imageSize
-//         }}
-//       />
-//     </>
-//   );
-// }
 
-// export default App;
+
+
+// const products = [
+//   {title: 'Cabbage', isFruit: false, id:1},
+//   {title: 'Garlic', isFruit: false, id:2},
+//   {title: 'Apple', isFruit: true, id:3},
+  
+// ];
+
+// export default function ShoppingList(){
+//   const listItems = products.map(product=>
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? 'pink' : 'darkgreen'
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   );
+
+//   return (
+//     <ul>
+//       {listItems}
+//     </ul>
+//   )
+// }
+function App() {
+  return (
+    <>
+      <h1>
+        Counters that update separately
+      </h1>
+      <MyButton/>
+      <MyButton/>
+    </>
+  );
+}
+
+export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ShoppingList from './App';
+
 // function AboutPage(){
 //   return(
 //     <>
@@ -20,7 +20,7 @@ import ShoppingList from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ShoppingList />
+    <App />
   </React.StrictMode>
 );
 
