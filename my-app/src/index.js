9fc0a2d057343458566baf11c58938cde9439b5f
Peter Huynh <trinh1234533@yahoo.com>
2024-02-18 21:11:51 -0600

    Added Hedy Lamarr

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // function AboutPage(){
@@ -15,10 +15,12 @@ import reportWebVitals from './reportWebVitals';
 // function HelloWorld(){
 //   return <h1 className= "greeting">Hello, World!</h1>
 // }
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <img className= "avatar" />
+    <App />
   </React.StrictMode>
 );
 
